95d79e6da3a542e5b50da2106db0b5b33ea0fdb0
amandat <amandatome@me.com>
2019-02-21 17:26:47 -0500

    More Media Queries

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,42 +7,42 @@ $(document).ready(function () {
     var questionIndex = 0;
     //Question, Answer, and Answer Choices================================================================
     var myQuestions = [
-        // {
-        //     question: "What is a group of flamingos called?",
-        //     answers: ["Flock", "Pride", "Flamboyance"],
-        //     correct: "Flamboyance",
-        //     funFact: "The backward bending 'knee' of a flamingo's leg is actually the bird's ankle. The actual knee is very close to the body and is not visible through the bird's plumage.",
-        // },
-        // {
-        //     question: "Which is not a term for a group of vultures?",
-        //     answers: ["Funeral", "Wake", "Venue"],
-        //     correct: "Funeral",
-        //     funFact: "Vultures pee on their legs to cool off in hot weather.",
-        // },
-        // {
-        //     question: "Penguins can stay underwater up to how many minutes?",
-        //     answers: ["10 minutes", "20 minutes", "30 minutes"],
-        //     correct: "20 minutes",
-        //     funFact: "Emperor penguin males incubate their eggs for two months in the winter without eating while the females are at sea.",
-        // },
-        // {
-        //     question: "Owls have what type of vision?",
-        //     answers: ["Binocular", "Myopic", "Persbyopic"],
-        //     correct: "Binocular",
-        //     funFact: "An owl has three eyelids: one for blinking, one for sleeping, and one for keeping the eye clean and healthy.",
-        // },
-        // {
-        //     question: "What is the average lifespan of a wild turkey?",
-        //     answers: ["10-15 years", "2-4 years", "3-5 years"],
-        //     correct: "3-5 years",
-        //     funFact: "A wild turkey’s gobble can be heard up to one mile away and is a primary means for a tom to communicate with his harem.",
-        // },
-        // {
-        //     question: "Which is not a blue jay colour?",
-        //     answers: ["Purple", "Brown", "White"],
-        //     correct: "Brown",
-        //     funFact: "Blue jays communicate via loud screams and high-pitched calls. They are able to imitate sound of hawks, cats and humans.",
-        // },
+        {
+            question: "What is a group of flamingos called?",
+            answers: ["Flock", "Pride", "Flamboyance"],
+            correct: "Flamboyance",
+            funFact: "The backward bending 'knee' of a flamingo's leg is actually the bird's ankle. The actual knee is very close to the body and is not visible through the bird's plumage.",
+        },
+        {
+            question: "Which is not a term for a group of vultures?",
+            answers: ["Funeral", "Wake", "Venue"],
+            correct: "Funeral",
+            funFact: "Vultures pee on their legs to cool off in hot weather.",
+        },
+        {
+            question: "Penguins can stay underwater up to how many minutes?",
+            answers: ["10 minutes", "20 minutes", "30 minutes"],
+            correct: "20 minutes",
+            funFact: "Emperor penguin males incubate their eggs for two months in the winter without eating while the females are at sea.",
+        },
+        {
+            question: "Owls have what type of vision?",
+            answers: ["Binocular", "Myopic", "Persbyopic"],
+            correct: "Binocular",
+            funFact: "An owl has three eyelids: one for blinking, one for sleeping, and one for keeping the eye clean and healthy.",
+        },
+        {
+            question: "What is the average lifespan of a wild turkey?",
+            answers: ["10-15 years", "2-4 years", "3-5 years"],
+            correct: "3-5 years",
+            funFact: "A wild turkey’s gobble can be heard up to one mile away and is a primary means for a tom to communicate with his harem.",
+        },
+        {
+            question: "Which is not a blue jay colour?",
+            answers: ["Purple", "Brown", "White"],
+            correct: "Brown",
+            funFact: "Blue jays communicate via loud screams and high-pitched calls. They are able to imitate sound of hawks, cats and humans.",
+        },
         {
             question: "What is a baby puffin called?",
             answers: ["Puffling", "Duckling", "Kindle"],
