7d6ca73ecb7ac7a58b1c9ffff7d59ce4f55f4fa6
amandat <amandatome@me.com>
2019-02-21 07:50:44 -0500

    Getting Answser

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -93,71 +93,74 @@ $(document).ready(function () {
     //ANSWER OPTIONS===============================================
     function answerOptions() {
         for (var i = 0; i < 3; i++) {
-            var answerBtn = $("<button>");
-            answerBtn.addClass("buttonClicked");
-            answerBtn.attr("data-answer", myQuestions[questionIndex].answers[i]);
-            answerBtn.text(myQuestions[questionIndex].answers[i]);
-            $("#answers").append(answerBtn);
-            $(".buttonClicked").on("click", function(){
-                answerBtn.text($(this).attr("data-answer"));
-            })
+            document.querySelector("#answers").innerHTML += ("<button id='opt" + i + "' data-answer='" + myQuestions[questionIndex].answers[i] + "'>" + myQuestions[questionIndex].answers[i] + "</button>");
+            $("button").addClass("buttonClicked");
+            console.log(myQuestions[questionIndex].correct);
         }
     }
 
     console.log(myQuestions[questionIndex].correct);
 
-    // document.querySelector("#answers").innerHTML += ("<button name='opt" + i + "' data-answer='" + myQuestions[questionIndex].answers[i] + "'>" + myQuestions[questionIndex].answers[i] + "</button>");
-    // $("button").addClass("buttonClicked");
-    //console.log(myQuestions[questionIndex].answers[i])
-    // document.querySelector("#answers").innerHTML += ("<input type='button' name='opt" + i + "' value='" + myQuestions[questionIndex].answers[i] + "'>" + "</input>");
 
-
-    //DISABLE BUTTONS===================================  
-    $("#answers").on("click", function () {
-        clearInterval(timer);
-        $(".buttonClicked").prop("disabled", true);
-        setTimeout(function () {
-            $(".buttonClicked").prop("disabled", false);
-        }, 4000);
-    });
 
     //CHECK ANSWERS====================================
     //Not working
     function checkAnswers() {
-        $(".buttonClicked").on("click", function () {
-            
-            
-            
-            // if (section.button[i + myQuestions[questionIndex].correct].clicked) {
-            //     if (section.button[i + myQuestions[questionIndex].correct].attr === myQuestions[questionIndex].correct){
-            //         alert("correct");
-            //     }
-            // }                
-                
-                
-                // answerBtn.text($(this).attr("data-answer")) === myQuestions[questionIndex].correct) {
-                // alert("hello");
+        $("#answers").on("click", function () {
+            var optionZero = document.querySelector("#opt0");
+            //console.log(optionZero);
+            var optionOne = document.querySelector("#opt1");
+            //console.log(optionOne);
+            var optionTwo = document.querySelector("#opt2");
+            //console.log(optionTwo.textContent);
 
-                // clearInterval(timer);
-                // document.querySelector("#question").innerHTML = ("Excellent. You are correct!");
-                // $("#answers").html("");
-                // setTimeout(loadQuestions, 4000);
-                // correctAnswers++;
-                // questionIndex++;
-            //}
-            // else {
-            //     clearInterval(timer);
-            //     document.querySelector("#question").innerHTML = ("Sorry. The correct answer is <br><button>" + myQuestions[questionIndex].correct + "</button>");
-            //     $("#answers").html("");
-            //     setTimeout(loadQuestions, 4000);
-            //     wrongAnswers++;
-            //     questionIndex++;
-            // }
+            if (optionZero.textContent === myQuestions[questionIndex].correct) {
+                clearInterval(timer);
+                document.querySelector("#question").innerHTML = ("Excellent. You are correct!");
+                $("#answers").html("");
+                setTimeout(loadQuestions, 4000);
+                correctAnswers++;
+                questionIndex++;
+            }
+            else if (optionOne.textContent === myQuestions[questionIndex].correct) {
+                clearInterval(timer);
+                document.querySelector("#question").innerHTML = ("Excellent. You are correct!");
+                $("#answers").html("");
+                setTimeout(loadQuestions, 4000);
+                wrongAnswers++;
+                questionIndex++;
+            }
 
+            else if (optionTwo.textContent === myQuestions[questionIndex].correct) {
+                clearInterval(timer);
+                document.querySelector("#question").innerHTML = ("Excellent. You are correct!");
+                $("#answers").html("");
+                setTimeout(loadQuestions, 4000);
+                correctAnswers++;
+                questionIndex++;
+            }
+            else {
+                clearInterval(timer);
+                document.querySelector("#question").innerHTML = ("Sorry. The correct answer is <br><button>" + myQuestions[questionIndex].correct + "</button>");
+                $("#answers").html("");
+                setTimeout(loadQuestions, 4000);
+                wrongAnswers++;
+                questionIndex++;
+            }
         });
     }
     checkAnswers();
     //To pass data attribute - If button has data-answer === myQuestions[questionIndex].correct then do this
+
+    //DISABLE BUTTONS===================================  
+    // $("#answers").on("click", function () {
+    //     clearInterval(timer);
+    //     $(".buttonClicked").prop("disabled", true);
+    //     setTimeout(function () {
+    //         $(".buttonClicked").prop("disabled", false);
+    //     }, 4000);
+    // });
+
 
     //RESULTS=============================================
     function results() {
